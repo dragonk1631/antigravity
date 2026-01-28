3ebf540af6420a35666de3ab0f6b8f520b448a8e
dragonk1379 <dragonk1379@gmail.com>
2026-01-29 02:11:49 +0900

    style: revert tileset to 32x32 and fix stair clipping by matching aspect ratio

--- games/SkyDash/src/objects/Stair.js
+++ games/SkyDash/src/objects/Stair.js
@@ -14,13 +14,12 @@ class Stair extends Phaser.GameObjects.Container {
 
         // 1. 타일셋 이미지가 지정된 경우 해당 타일 사용, 없으면 Graphics 텍스처 사용
         if (this.tileFrame !== null && scene.textures.exists('tileset')) {
-            this.stairImage = scene.add.sprite(0, 0, 'tileset', this.tileFrame);
+            this.stairImage = scene.add.sprite(0, this.stepHeight, 'tileset', this.tileFrame);
 
-            // 픽셀 아트 잘림 방지 및 계단 간의 풍성한 연결을 위해 112x112(정수배) 설정
-            // 상단 80px은 밟는 구역, 하단 32px은 아래 계단과의 이음새를 덮는 여유분입니다.
-            this.stairImage.setDisplaySize(112, 112);
-            this.stairImage.setOrigin(0.5, 0);
-            this.stairImage.y = -16; // 약간 위로 배치하여 이음새를 더 자연스럽게 만듦
+            // 32x32 타일을 계단 너비(100)에 맞춰 1:1 비율(100x100)로 표시
+            // 하단(y=80)을 기준으로 정렬하여 계단 바닥면을 맞춤 (잘림 방지)
+            this.stairImage.setOrigin(0.5, 1);
+            this.stairImage.setDisplaySize(this.stepWidth, 100);
         } else {
             // 정적 텍스처 생성 (한 번만 - 성능 최적화) 폴백
             if (!scene.textures.exists('stair_texture')) {
@@ -115,9 +114,9 @@ class Stair extends Phaser.GameObjects.Container {
         // 타일 프레임 갱신
         if (tileFrame !== null && this.scene.textures.exists('tileset')) {
             this.stairImage.setTexture('tileset', tileFrame);
-            this.stairImage.setDisplaySize(112, 112);
-            this.stairImage.setOrigin(0.5, 0);
-            this.stairImage.y = -16;
+            this.stairImage.setDisplaySize(this.stepWidth, 100);
+            this.stairImage.setOrigin(0.5, 1);
+            this.stairImage.y = this.stepHeight;
 
             // 377번 계단 타일의 좌우 반전 처리
             // direction 1(오른쪽)일 때와 -1(왼쪽)일 때 계단 모양을 맞춤
